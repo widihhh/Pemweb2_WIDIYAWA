c8ae657904f16d1a62dc2be64cf0beea8dec1403
widiyawati <91334320+widihhh@users.noreply.github.com>
2022-04-16 14:26:08 +0700

    repost

---
+++ Praktikum-3/template-html/atas.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>SAS_STTNF</title>
+</head>
+<body>
+    
+    <header role="banner">
+    <h2>Student Activity Score - STTNF</h2>
+    <a href="#">Home</a> |
+    <a href="#">Activity</a> | 
+    <a href="#">My Score</a> |
+    <a href="#">Login</a>
+    <hr>
+    </header>
